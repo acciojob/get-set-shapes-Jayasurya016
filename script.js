63b573ed93ec4bd0be91c74a947a334743eec71d
JAYASURYA KADAKUZHILL JOB <161327970+Jayasurya016@users.noreply.github.com>
2025-09-01 15:19:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ console.log(rectangle.height); // Output: 10
 console.log(rectangle.getArea()); // Output: 50
 
 // Creating a Square instance
-const square = new Square(7);
+const square = new Square(7,7);
 console.log(square.width); // Output: 7
 console.log(square.height); // Output: 7
 console.log(square.getArea()); // Output: 49
