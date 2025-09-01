d825618515402f047a2d0da319ff2a603fb32ae7
JAYASURYA KADAKUZHILL JOB <161327970+Jayasurya016@users.noreply.github.com>
2025-09-01 15:23:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ class Rectangle {
 	}
 }
 
-class Square extends Animal {
+class Square extends Rectangle {
 	 constructor(side){
         super(side,side)
         this.side = side
@@ -35,7 +35,7 @@ console.log(rectangle.height); // Output: 10
 console.log(rectangle.getArea()); // Output: 50
 
 // Creating a Square instance
-const square = new Square(7,7);
+const square = new Square(7);
 console.log(square.width); // Output: 7
 console.log(square.height); // Output: 7
 console.log(square.getArea()); // Output: 49
